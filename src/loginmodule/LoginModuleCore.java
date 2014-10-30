42f525be712339d9e52162fde0fa507652aaab86
Nisarg Shah <nis999999@gmail.com>
2014-10-30 22:33:41 +0530

    completed login module

---
+++ src/loginmodule/LoginModuleCore.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loginmodule;
+
+import java.io.IOException;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+/**
+ *
+ * @author nisarg
+ */
+public class LoginModuleCore {
+
+    public static String connect(String usrname, String pwd) {
+        
+        String username = usrname;
+        String password = pwd;
+        boolean status = false;
+        
+        try {
+            
+            Connection.Response loginForm = Jsoup
+                    .connect("http://10.1.1.242/moodle/login/index.php")
+                    .method(Connection.Method.GET)
+                    .execute();
+
+            Document document = Jsoup
+                    .connect("http://10.1.1.242/moodle/login/index.php")
+                    .data("username", username)
+                    .data("password", password)
+                    .cookies(loginForm.cookies())
+                    .post();
+            
+            status = document.body().html().contains("You are logged in as");
+
+        } catch (IOException e) {
+            
+            System.out.println("IO exception!");
+            
+        } finally {
+           
+            return String.valueOf(status);
+            
+        }
+    }
+
+}
