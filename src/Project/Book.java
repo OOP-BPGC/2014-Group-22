fdd4f5bb67a1825a3633339ae2f41689927389b3
Varsheeth <f2013045@goa.bits-pilani.ac.in>
2014-11-11 16:25:55 +0530

    Modified

--- src/Project/Book.java
+++ src/Project/Book.java
@@ -1,7 +1,7 @@
 package Project;
 abstract class Book
 {
-	public String generateUID();
+	public abstract String generateUID();
 	public abstract void generateForm();
 	public abstract void displayStatus(String UID);
 	public abstract void cancelRequest(String UID);
